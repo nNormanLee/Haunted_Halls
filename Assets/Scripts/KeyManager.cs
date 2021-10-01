6244da5c588389ab36276d78f24c4ef2a0c408bc
Bon <bonbienvenu@yahoo.com>
2021-10-01 15:23:04 -0600

    Particle and Script

---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int whichKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //Button Key
+        if(whichKey == 0)
+        {
+            gameObject.transform.parent.SetActive(True);
+        }
+        //Voice Key
+        else if(whichKey == 1)
+        {
+
+        }
+        //Other Key
+        else if(whichKey == 2)
+        {
+
+        }
+        //Last Key
+        else
+        {
+
+        }
+    }
+}
